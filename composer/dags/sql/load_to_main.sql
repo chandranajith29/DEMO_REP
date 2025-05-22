e12328a8dd1a5b0722d4dc509f10d1c581ed8355
chandranajith29 <chandranajith29@gmail.com>
2025-05-23 00:13:06 +0530

    change

--- composer/dags/sql/load_to_main.sql
+++ composer/dags/sql/load_to_main.sql
@@ -27,8 +27,6 @@ SELECT
     cast(edi_order_number as INTEGER),
     impressions,
   CURRENT_TIMESTAMP() AS bq_insertion_time,
-  @dag_id AS dag_id,
-  @dag_run_id AS dag_run_id,
   ARRAY_REVERSE(SPLIT(_FILE_NAME, '/'))[SAFE_OFFSET(0)] AS file_name
 FROM
   mvpd_data.mvpd_spectrum_stg
